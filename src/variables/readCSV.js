ef063e473e605b2e99ded093748c5a53d87e83c0
meowie2k3 <dmhuy.ityu@gmail.com>
2024-05-10 23:09:00 +0700

    read data from github raw content

--- src/variables/readCSV.js
+++ src/variables/readCSV.js
@@ -1,17 +1,19 @@
 import Papa from 'papaparse';
-const dataPath = './AAPL.csv';
+
+const dataLink = "https://raw.githubusercontent.com/meowie2k3/DSDV-2024/main/src/assets/csv/AAPL.csv?token=GHSAT0AAAAAACQ2A2SLO3HHAEO57VLGLHOYZR6KQVA"
+
+
 
 
 // Import PapaParse library
 
 function readCSV(){
-    // Read CSV file
-    fetch( dataPath )
-    .then( response => response.text() )
-    .then( responseText => {
-        // -- parse csv
-        var data = Papa.parse(responseText);
-        console.log('data:', data);
+    Papa.parse(dataLink, {
+        download: true,
+        header: true,
+        complete: function(results) {
+        console.log(results);
+        }
     });
 }
 export default readCSV;
