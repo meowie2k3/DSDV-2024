707ea8681e8e405a8468ab1148796118edb99872
minhhhuydao2003 <daominhhuy.9a1@gmail.com>
2024-05-11 09:54:40 +0700

    setup function to read csv file

--- src/variables/readCSV.js
+++ src/variables/readCSV.js
@@ -1,19 +1,19 @@
-import Papa from 'papaparse';
+import Papa from "papaparse";
 
-const dataLink = "https://raw.githubusercontent.com/meowie2k3/DSDV-2024/main/src/assets/csv/AAPL.csv?token=GHSAT0AAAAAACQ2A2SLO3HHAEO57VLGLHOYZR6KQVA"
-
-
-
+const dataLink =
+  "https://raw.githubusercontent.com/meowie2k3/DSDV-2024/main/src/assets/csv/AAPL.csv?token=GHSAT0AAAAAACRZPXCINBEHNXBIWTE54V4GZR64EQA";
 
 // Import PapaParse library
 
-function readCSV(){
-    Papa.parse(dataLink, {
-        download: true,
-        header: true,
-        complete: function(results) {
-        console.log(results);
-        }
-    });
+async function readCSV() {
+    // Fetch the CSV file
+    const response = await fetch(dataLink);
+    const reader = response.body.getReader();
+    const result = await reader.read(); // raw array
+    const decoder = new TextDecoder("utf-8");
+    const csv = decoder.decode(result.value); // the csv text
+    const results = Papa.parse(csv, { header: true }); // object with { data, errors, meta }
+    const rows = results.data; // array of objects
+    return rows;
 }
 export default readCSV;
